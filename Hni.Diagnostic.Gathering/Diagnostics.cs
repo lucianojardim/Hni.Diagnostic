fd44513490d8fc58c7de467c2bc0ba38c5cb01f1
JardimL <JardimL@WKSLT-123053>
2016-06-16 15:03:51 -0500

    Initial commit

---
+++ Hni.Diagnostic.Gathering/Diagnostics.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Hni.Diagnostic.Gathering
+{
+    public class DiagnosticInformation
+    {
+        public DiagnosticInformation()
+        {
+        }
+        public DiagnosticInformation(string diagnosticSource, string diagnosticDescription, string diagnosticData)
+        {
+            DiagnosticSource = diagnosticSource;
+            DiagnosticDescription = diagnosticDescription;
+            DiagnosticData = diagnosticData;
+        }
+        public DiagnosticInformation(string diagnosticSource, string diagnosticData)
+        {
+            DiagnosticSource = diagnosticSource;
+            DiagnosticDescription = null;
+            DiagnosticData = diagnosticData;
+        }
+
+        public string DiagnosticSource { get; set; }
+        public string DiagnosticDescription { get; set; }
+        public string DiagnosticData { get; set; }
+
+        public void Save()
+        {
+            if (DiagnosticSource == null)
+            {
+                DiagnosticSource = "The caller didn't identified itself.";
+            }
+
+            if (DiagnosticData == null)
+            {
+                DiagnosticData = "No diagnostic information was sent.";
+            }
+
+            try
+            {
+                HniDiagnosticEntities hniDiagnosticEntities = new HniDiagnosticEntities();
+
+                Diagnostic diagnostic = new Diagnostic();
+                diagnostic.DiagnosticSource = DiagnosticSource;
+                diagnostic.DiagnosticDateTime = System.DateTime.Now;
+                diagnostic.DiagnosticDescription = DiagnosticDescription;
+                diagnostic.DiagnosticData = DiagnosticData;
+
+                hniDiagnosticEntities.Diagnostics.Add(diagnostic);
+                hniDiagnosticEntities.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Hni.Diagnostic.TestConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hni.Diagnostic.Gathering;
+
+namespace Hni.Diagnostic.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Test1");
+                DiagnosticInformation diagnosticInformation = new DiagnosticInformation("Hni.Diagnostic.TestConsole.Program.Main", "Testing1", "nothing1");
+                diagnosticInformation.Save();
+
+                Console.WriteLine("Test2");
+                DiagnosticInformation diagnosticInformation2 = new DiagnosticInformation("Hni.Diagnostic.TestConsole.Program.Main", "nothing2");
+                diagnosticInformation2.Save();
+
+                Console.WriteLine("Test3");
+                diagnosticInformation.DiagnosticDescription = "Testing2";
+                diagnosticInformation.DiagnosticData = "nothing2";
+                diagnosticInformation.Save();
+
+                int x = Console.Read();
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                int x = Console.Read();
+            }
+        }
+    }
+}
