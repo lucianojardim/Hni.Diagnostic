3bb8f7c52735c44f7aee19f914acca8656c3273f
JardimL <JardimL@WKSLT-123053>
2016-06-16 09:25:27 -0500

    Add project files.

---
+++ Hni.Diagnostic.Gathering/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hni.Diagnostic.Gathering
+{
+    public class Class1
+    {
+    }
+}
